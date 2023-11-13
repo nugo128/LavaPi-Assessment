451943fa4e3d725ea5d9c0fd244ef18811920e2a
nugo128 <nugo.rostiashvili@redberry.ge>
2023-11-13 16:47:05 +0400

    style: responsive design for homepage

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center py-3 gap-8">
+  <div class="hidden md:flex items-center justify-center py-3 gap-8">
     <RouterLink
       to="/"
       class="text-3xl hover:font-bold text-center w-20 hover:text-light-yellow"
@@ -25,10 +25,58 @@
       >Create User</RouterLink
     >
   </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="w-10 h-10 ml-2 mt-2 md:hidden block z-20"
+    @click="toggleHamburger"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+  </svg>
+  <div
+    v-show="hamburgerActive"
+    class="md:hidden flex absolute flex-col justify-center py-3 px-10 gap-8 bg-gray-600 h-screen z-10"
+  >
+    <RouterLink
+      to="/"
+      class="text-3xl hover:font-bold text-center w-20 hover:text-light-yellow"
+      :class="path.fullPath === '/' ? 'text-light-yellow' : ''"
+      >Home</RouterLink
+    >
+    <RouterLink
+      to="/list"
+      class="text-3xl hover:font-bold w-32 text-center hover:text-light-yellow"
+      :class="path.fullPath === '/list' ? 'text-light-yellow' : ''"
+      >Users List</RouterLink
+    >
+    <RouterLink
+      to="/details"
+      class="text-3xl hover:font-bold w-40 text-center hover:text-light-yellow"
+      :class="path.fullPath === '/details' ? 'text-light-yellow' : ''"
+      >User Details</RouterLink
+    >
+    <RouterLink
+      to="/create"
+      class="text-3xl hover:font-bold w-40 text-center hover:text-light-yellow"
+      :class="path.fullPath === '/create' ? 'text-light-yellow' : ''"
+      >Create User</RouterLink
+    >
+  </div>
 </template>
 <script setup>
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+const hamburgerActive = ref(false)
+const toggleHamburger = () => {
+  hamburgerActive.value = !hamburgerActive.value
+}
 const route = useRoute()
 const path = ref(route)
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="flex justify-center items-center flex-col gap-12 mt-48">
+    <div class="custom-shape bg-light-yellow top-20 md:left-1/3 right-1/3 absolute w-32"></div>
+    <div
+      class="custom-shape-2 bg-light-yellow md:bottom-[18%] md:left-1/3 bottom-[72px] left-0 absolute md:w-24 w-32"
+    ></div>
+    <div
+      class="custom-shape-3 bg-light-yellow md:top-[40%] md:right-[10%] top-[73%] left-2/3 absolute md:w-44 w-32"
+    ></div>
     <img
       src="../../public/images/avatar1.jpg"
       alt="Your Image"
-      class="absolute bottom-52 right-[82%] w-48 rounded-full border-2 border-light-yellow"
+      class="absolute bottom-28 md:bottom-52 left-[2%] md:left-[18%] w-28 md:w-48 rounded-full border-2 border-light-yellow"
     />
-    <div class="custom-shape bg-light-yellow top-20 left-1/3 absolute w-32"></div>
     <img
       src="../../public/images/avatar2.jpeg"
       alt="Your Image"
-      class="absolute top-28 right-1/3 w-44 rounded-full border-2 border-light-yellow"
+      class="absolute top-28 right-1/3 w-28 md:w-44 rounded-full border-2 border-light-yellow"
     />
-    <div class="custom-shape-2 bg-light-yellow bottom-[18%] left-1/3 absolute w-24"></div>
-    <div class="custom-shape-3 bg-light-yellow top-[40%] right-[10%] absolute w-44"></div>
     <img
       src="../../public/images/avatar3.jpeg"
       alt="Your Image"
-      class="absolute top-3/4 left-2/3 w-40 rounded-full border-2 border-light-yellow"
+      class="absolute top-3/4 left-2/3 w-28 md:w-40 rounded-full border-2 border-light-yellow"
     />
 
     <h1
       :class="`${
         animate ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-20'
-      } transition-all duration-[3000ms] ease-in-out overflow-hidden text-6xl font-bold italic`"
+      } transition-all duration-[3000ms] ease-in-out overflow-hidden lg:text-6xl md:text-4xl text-3xl  font-bold text-center italic px-4`"
     >
       Welcome to User Management Application
     </h1>
     <h2
       :class="`${
         animate ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-32'
-      } transition-all duration-[4000ms] ease-in-out overflow-hidden text-4xl font-bold italic`"
+      } transition-all duration-[4000ms] ease-in-out overflow-hidden lg:text-4xl md:text-3xl text-2xl  font-bold text-center italic px-4`"
     >
       You can Add, Edit or Delete users here!
     </h2>
@@ -37,7 +41,7 @@
       to="/list"
       :class="`${
         animate ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-44'
-      } transition-all duration-[5000ms] ease-in-out overflow-hidden text-4xl font-bold bg-red-500 rounded-xl px-4 py-2`"
+      } transition-all duration-[5000ms] ease-in-out overflow-hidden md:text-4xl text-2xl font-bold bg-red-500 rounded-xl px-4 py-2`"
       >GET STARTED</RouterLink
     >
   </div>
