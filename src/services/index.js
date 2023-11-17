06772902ec89dc5275fa30aa23910846a65b1c10
nugo128 <nugo.rostiashvili@redberry.ge>
2023-11-17 11:57:53 +0400

    refactor: remove unused code

--- src/services/index.js
+++
@@ -1,9 +0,0 @@
-export async function getAllUsers() {
-  try {
-    const response = await fetch('https://dummyjson.com/users')
-    const data = await response.json()
-    return data
-  } catch (error) {
-    console.error('Error fetching users:', error)
-  }
-}
